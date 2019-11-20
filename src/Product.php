4ee679728fab9584885068f6cc7f22b48628e6b1
johannes <johannes.muljana@ait.nsw.edu.au>
2019-11-20 19:50:34 +1100

    added images to product

--- src/Product.php
+++ src/Product.php
@@ -12,11 +12,14 @@ class Product extends Database {
   public function getProducts() {
     $query = "
     SELECT
-    product_id,
+    @pid := product.product_id as product_id,
     name,
     description,
-    price
+    price,
+    ( SELECT @img_id := image_id FROM product_image WHERE product_id = @pid LIMIT 1 ) as image_id,
+    ( SELECT image_file_name FROM image WHERE image_id = @img_id ) as image
     FROM product
+    WHERE active = 1
     ";
     try{
       $statement = $this -> connection -> prepare( $query );
