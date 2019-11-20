8126bd7d86ade9c126ab954f13e2f3f124c2ddeb
johannes <johannes.muljana@ait.nsw.edu.au>
2019-11-20 18:24:39 +1100

    added images and moved db credentials

--- src/Database.php
+++ src/Database.php
@@ -3,10 +3,23 @@
 namespace oldspice;
 
 class Database {
+  private $host;
+  private $user;
+  private $password;
+  private $database;
   protected $connection;
 
   protected function __construct() {
-    $this -> connection = mysqli_connect("localhost","test","password","testdata");
+    $this -> host = getenv('host');
+    $this -> user = getenv('user');
+    $this -> password = getenv('password');
+    $this -> database = getenv('database');
+    $this -> connection = mysqli_connect(
+      $this -> host,
+      $this -> user,
+      $this -> password,
+      $this -> database
+    );
   }
 }
 ?>
