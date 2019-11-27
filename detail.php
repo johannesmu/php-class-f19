54fa2e36b5762930d57b48bbef5b230e83b3aa04
johannes <johannes.muljana@ait.nsw.edu.au>
2019-11-27 20:03:39 +1100

    added detail page

---
+++ detail.php
@@ -0,0 +1,25 @@
+<?php
+require('vendor/autoload.php');
+
+use oldspice\ProductDetail;
+
+if( $_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['product_id']) ) {
+  $product_id = $_GET['product_id'];
+  $pd = new ProductDetail();
+  $detail = $pd -> getDetail( $product_id );
+}
+else{
+  $detail = '';
+}
+
+//Twig
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment( $loader );
+//load the template
+$template = $twig -> load( 'detail.twig' );
+//output the template to page
+echo $template -> render([
+  'detail' => $detail,
+  'title' => 'Detail for ' . $detail['product']['name']
+]);
+?>
