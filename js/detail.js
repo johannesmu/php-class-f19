2a64a5e8d90a7b72b6d1c65fd094af19609d31be
johannes <jmuljana@gmail.com>
2020-02-04 01:02:34 +1100

    adding plus function to detail

--- js/detail.js
+++ js/detail.js
@@ -1,5 +1,12 @@
 $(document).ready( () => {
   // get the total avaible quantity
-  const available = parseInt( $('#available').text() )
-  console.log( available)
+  const maxQuantity = parseInt( $('#available').text() );
+  const minQuantity = 1;
+  const qtyField = $('#quantity');
+  // add listeners to plus
+  $('#plus').click( () => {
+    if( $(qtyField).val() <= maxQuantity && $(qtyField).val() >= minQuantity ) {
+      $(qtyField).val() += $(qtyField).val();
+    }
+  })
 })
