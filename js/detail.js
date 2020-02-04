efa4ca04a6b897ebbc4a898915b5d9c75b3ed911
johannes <jmuljana@gmail.com>
2020-02-04 12:24:29 +1100

    price update on button in detail twig

--- js/detail.js
+++ js/detail.js
@@ -2,10 +2,10 @@ const minQuantity = 1;
 // to be updated when page loads
 let maxQuantity = 0;
 let currentQty = 0;
-let itemPrice = 0;
 
 function updateTotalPrice( qty, price ) {
-
+  const total = parseInt(qty) * parseFloat(price);
+  $('#total-price').text( total.toFixed(2) );
 }
 
 $(document).ready( () => {
@@ -13,18 +13,20 @@ $(document).ready( () => {
   maxQuantity = parseInt( $('#available').text() );
   const qtyField = $('#quantity');
   currentQty = $(qtyField).val();
-  itemPrice = parseFloat( $('.price').text() );
+  const itemPrice = parseFloat( $('#product-price').text() );
   // add listeners to plus
   $('#plus').click( () => {
     if( currentQty < maxQuantity && currentQty >= minQuantity ) {
       currentQty++;
       $(qtyField).val(currentQty);
+      updateTotalPrice( currentQty, itemPrice );
     }
   })
   $('#minus').click( () => {
     if( currentQty <= maxQuantity && currentQty > minQuantity ) {
       currentQty--;
       $(qtyField).val(currentQty);
+      updateTotalPrice( currentQty, itemPrice );
     }
   })
 })
