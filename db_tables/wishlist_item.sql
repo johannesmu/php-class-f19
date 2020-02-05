173352b363e05beb6040c7b6ffd4c15302345394
johannes <jmuljana@gmail.com>
2020-02-05 12:57:42 +1100

    update to wishlist_item table

--- db_tables/wishlist_item.sql
+++ db_tables/wishlist_item.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 05, 2020 at 12:01 PM
+-- Generation Time: Feb 05, 2020 at 12:57 PM
 -- Server version: 10.1.43-MariaDB-0ubuntu0.18.04.1
 -- PHP Version: 7.2.24-0ubuntu0.18.04.2
 
@@ -32,7 +32,8 @@ CREATE TABLE `wishlist_item` (
   `wishlist_item_id` int(11) NOT NULL,
   `wishlist_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `active` int(11) NOT NULL
+  `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `active` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
